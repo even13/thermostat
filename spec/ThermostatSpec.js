a6151793c3818db1d32bc8c3e38d412a999b2997
Eve Noirault <student@Makerss-MacBook-Air.local>
2019-12-11 16:12:07 +0000

    add test for power saving off

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -53,4 +53,14 @@ describe('Thermostat', function() {
     });
   });
 
+  describe('when power saving mode is off', function() {
+    it('has a maximum temperature of 32 degrees', function() {
+      thermostat.switchPowerSavingModeOff();
+      for (var i = 0; i < 13; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(32);
+    });
+  });
+
 });
